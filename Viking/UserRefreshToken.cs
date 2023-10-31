aa2e91e7a70e0f86df2f750fbb5c0576dfd515c3
Andrey <avrulev@sibintec>
2023-10-31 22:21:13 +0700

    add conDataBase

---
+++ Viking/UserRefreshToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Viking;
+
+public partial class UserRefreshToken
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string RefreshToken { get; set; } = null!;
+
+    public string RefreshTokenExpiryTime { get; set; } = null!;
+
+    public string RefreshTokenCreatedTime { get; set; } = null!;
+}
